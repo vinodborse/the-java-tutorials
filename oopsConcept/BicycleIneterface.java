26c53390400d86545f2068ec3598a4f1798b01bd
gauravkukade <gkukade54@gmail.com>
2017-11-08 16:25:14 +0530

    Add oopsConcept

---
+++ oopsConcept/BicycleIneterface.java
@@ -0,0 +1,12 @@
+package oopsConcept;
+
+public interface BicycleIneterface {
+	
+	//wheel revolutions per minute
+	
+	void changeCadence(int newValue);
+	void speedUp(int increment);
+	void changeGear(int newValue);
+	void applyBrakes(int decrement);
+
+}
