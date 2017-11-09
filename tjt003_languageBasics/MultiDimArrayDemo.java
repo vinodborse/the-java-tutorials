a641201bf65313e71834150760005123b013030b
gauravkukade <gkukade54@gmail.com>
2017-11-09 14:34:21 +0530

    Update MultiDimArrayDemo

--- tjt003_languageBasics/MultiDimArrayDemo.java
+++ tjt003_languageBasics/MultiDimArrayDemo.java
@@ -1,5 +1,18 @@
 package tjt003_languageBasics;
 
+
 public class MultiDimArrayDemo {
 
+	public static void main(String[] args) {
+		
+		String[][] names= {
+				{"Mr. ","Mrs. ","Ms. "},
+				{"James", "Smith"}
+		};
+		
+		//Mr. James
+		System.out.println(names[0][0]+names[1][0]);
+		//Ms. Smith
+		System.out.println(names[0][2]+names[1][1]);
+	}
 }
